16ddf5841a4296a44809347412685e4343bbd1dd
lfoIncluit <luciano.fernandez@incluit.com>
2022-11-02 10:00:53 -0300

    motor de inferencia para deteccion de rasgos faciales agregado.

--- src/main.py
+++ src/main.py
@@ -1,33 +1,48 @@
 import cv2
 from imutils import resize
-from motores_de_inferencia import Detector_de_rostros, Identificador_de_rostros
+import motores_de_inferencia as mi
 from objetos import Imagen, Rostro
 
 RED = (0, 0, 255)  # bounding-rect color
 ancho_recta = 2
 
+# Modelo de deteccion de rostros
 dr_model_bin = "../modelos/face-detection-retail-0004/face-detection-retail-0004.bin"
 dr_model_xml = "../modelos/face-detection-retail-0004/face-detection-retail-0004.xml"
+
+# Modelo de identificacion de rostros
 ir_model_bin = "../modelos/face-reidentification-retail-0095/face-reidentification-retail-0095.bin"
 ir_model_xml = "../modelos/face-reidentification-retail-0095/face-reidentification-retail-0095.xml"
+
+# Modelo de deteccion de rasgos faciales
+drf_model_bin = "../modelos/facial-landmarks-98-detection-0001/facial-landmarks-98-detection-0001.bin"
+drf_model_xml = "../modelos/facial-landmarks-98-detection-0001/facial-landmarks-98-detection-0001.xml"
+
+
 CHOFERES_PATH = "../imagenes_rostros_conductores"
 VIDEO_PATH = 0
 device = "CPU"
-confidence_threshold = 0.8
+confidence_threshold = 0.6
 
 def main():
 
-    detector_de_rostros = Detector_de_rostros(dr_model_xml, dr_model_bin, device, confidence_threshold)
-    identificador_de_rostros = Identificador_de_rostros(ir_model_xml, ir_model_bin, device, confidence_threshold)
+    # Creamos los motores de inferencia
+    detector_de_rostros = mi.Detector_de_rostros(dr_model_xml, dr_model_bin, device, confidence_threshold)
+    identificador_de_rostros = mi.Identificador_de_rostros(ir_model_xml, ir_model_bin, device, confidence_threshold)
+    detector_de_rasgos_faciales = mi.Detector_de_rasgos_faciales(drf_model_xml, drf_model_bin, device, confidence_threshold)
+    
     identificador_de_rostros.generar_base_de_datos_de_choferes(CHOFERES_PATH)
     vidcap = cv2.VideoCapture(VIDEO_PATH)
     success, img = vidcap.read()
     while success:
-        rostro = detector_de_rostros.procesar_frame(img)
+        rostro : Rostro = detector_de_rostros.procesar_frame(img)
         if rostro.rostro_detectado:
             imagen_rostro_recortado = Imagen.obtener_imagen_rostro_recortado(img, rostro)
             if rostro.nombre == "DESCONOCIDO":
                 rostro.nombre = identificador_de_rostros.obtener_nombre_conductor(imagen_rostro_recortado)
+
+
+            drf_results = detector_de_rasgos_faciales.procesar_frame(imagen_rostro_recortado, img.shape, rostro.location["tl"])
             cv2.rectangle(img, rostro.location["tl"], rostro.location["br"], RED, ancho_recta)
             input_height, input_width, _ = img.shape
             cv2.putText(
@@ -35,11 +50,12 @@ def main():
                 rostro.nombre,
                 (int(input_width / 4), int(input_height / 12)),
                 cv2.FONT_HERSHEY_SIMPLEX,
-                3,
-                (255, 255, 255),
+                1,
+                RED,
                 2,
             )
-
+            for point in drf_results:
+                cv2.circle(img, (int(point[1]), int(point[0])), 1 + int(0.0012 * 64), (255, 0, 0), -1)
         showImg = resize(img, height=750, width=680)
         cv2.imshow("showImg", showImg)
         cv2.waitKey(1)
@@ -47,7 +63,6 @@ def main():
             break
         success, img = vidcap.read()
     print("Programa terminado")
-
 
 if __name__ == "__main__":
     main()
--- src/motores_de_inferencia.py
+++ src/motores_de_inferencia.py
@@ -47,23 +47,23 @@ class Motor_de_inferencia:
             frame, size=(self.image_prop.height, self.image_prop.width), ddepth=cv2.CV_8U
         )
 
-    def procesar_frame(self, blob) -> dict:
+    def procesar_frame(self, frame) -> dict:
         """[summary]
         :param frame: frame blob
         :type frame: numpy.ndarray
         :rtype: (bool, numpy.ndarray, str)
         """
-        return self.execution_net.infer(inputs={self.input_blob: blob}).get(
+        self.blob = self.redimensionar_imagen(frame)
+        return self.execution_net.infer(inputs={self.input_blob: self.blob}).get(
             self.output_blob
         )
 
 class Detector_de_rostros(Motor_de_inferencia):
     
-    def procesar_frame(self, frame):
+    def procesar_frame(self, frame) -> Rostro:
         input_height, input_width, _ = frame.shape
-        blob = self.redimensionar_imagen(frame)
         self.rostro = Rostro.getInstance()
-        self.rostro.actualizar_atributos(super().procesar_frame(blob)[0][0][0])
+        self.rostro.actualizar_atributos(super().procesar_frame(frame)[0][0][0])
         
         if self.rostro.confidence < self.confidence_threshold:
             self.log.warning(f"Face detection less than {self.confidence_threshold}, accuracy {self.rostro.confidence}")
@@ -82,16 +82,16 @@ class Detector_de_rostros(Motor_de_inferencia):
         
 class Identificador_de_rostros(Motor_de_inferencia):
 
+    """
     def redimensionar_imagen(self, frame):
         resized_frame = cv2.resize(frame, (self.image_prop.width, self.image_prop.height))
 
         # reshape to network input shape
         # Change data layout from HWC to CHW
         return expand_dims(resized_frame.transpose(2, 0, 1), 0)
-
+    """
     def procesar_frame(self, frame) -> list:
-        blob = self.redimensionar_imagen(frame)
-        return [x[0][0] for x in list(super().procesar_frame(blob)[0])]
+        return [x[0][0] for x in list(super().procesar_frame(frame)[0])]
 
     def generar_base_de_datos_de_choferes(self, directorio_imagenes_choferes : str):
         self.choferes_dict = {}
@@ -115,3 +115,46 @@ class Identificador_de_rostros(Motor_de_inferencia):
             if result >= self.confidence_threshold:
                 return name
         return "DESCONOCIDO"
+
+class Detector_de_rasgos_faciales(Motor_de_inferencia):
+
+    def sign(self, number : float):
+        if number > 0:
+            return 1
+        elif number < 0:
+            return -1
+        else:
+            return 0
+
+    def procesar_frame(self, face_frame, frame_shape, posicion_inicial):
+        frame_height, frame_width, _ = frame_shape
+        x_inicial = posicion_inicial[0]
+        y_inicial = posicion_inicial[1]
+        drf_result = super().procesar_frame(face_frame)[0]
+        position_points = []
+        rows, colums = drf_result[0].shape
+        for point in drf_result:
+            _value = -1
+            x_position = 0
+            y_position = 0
+            for x in range(colums):
+                for y in range(rows):
+                    if point[x][y] > _value:
+                        _value = point[x][y]
+                        x_position = x
+                        y_position = y 
+            if _value > 0:
+                position_points.append([x_position, y_position])
+            else:
+                position_points.append([-1,-1])
+        for index, coord in enumerate(position_points):
+            if 1 < coord[0] and coord[0] < colums - 1 and 1 < coord[1] and coord[1] < rows - 1:
+                diffFirst = drf_result[index][coord[0]+1][coord[1]] - drf_result[index][coord[0]-1][coord[1]]
+                diffSecond = drf_result[index][coord[0]][coord[1]+1] - drf_result[index][coord[0]][coord[1]-1]
+                coord[0] += self.sign(diffFirst) * 0.25
+                coord[1] += self.sign(diffSecond) * 0.25
+            coord[0] =  y_inicial + int(frame_width * coord[0] / self.image_prop.width)
+            coord[1] = x_inicial + int(frame_height * coord[1] / self.image_prop.height)
+        return position_points
+
+
